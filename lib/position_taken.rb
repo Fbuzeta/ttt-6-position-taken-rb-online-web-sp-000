907b2b60c7971538fff74d05178c4f72995e9a0b
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2019-10-28 00:00:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,7 @@
 def position_taken?(board, index)
-  (board[index].nil? || board[index] == " ")
-end# code your #position_taken? method here!
-
-def position_taken?(board, index)
-  (board[index].nil? || board[index] == " ")
+  !(board[index].nil? || board[index] == " ")
 end
 
-def position_taken?(board, index)
-  (board[index].nil? || board[index] == "")
+def position_taken(board, index)
+  !(board[index].nil?) || board[index] == "")
 end
